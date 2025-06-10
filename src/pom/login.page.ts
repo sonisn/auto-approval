cc95254649b3c5b273258f53b3fa6269c1a9091f
Snehal Soni <snehal.soni@farohealth.com>
2025-06-09 21:42:55 -0500

    Make sure Login Page is fully loaded and functional.

--- src/pom/login.page.ts
+++ src/pom/login.page.ts
@@ -31,6 +31,15 @@ export class LoginPage {
         // Store Username and Password in GitHub Action Secret and Read to here to user for login.
         const userName = process.env.USERNAME;
         const password = process.env.PASSWORD;
+        // Login Page takes a while to fully load and processed and especially on Github Actions.
+        // Wait for the login page to fully load.
+        await this.page.waitForLoadState("load");
+        await this.page.waitForLoadState("networkidle");
+        await this.page.waitForLoadState("domcontentloaded");
+        // Wait for the login page to be visible.
+        await this.loginWithBapsSSOUserName.waitFor({ state: "visible" });
+        await this.loginWithBapsSSOPassword.waitFor({ state: "visible" });
+        await this.signInBtn.waitFor({ state: "visible" });
         // Check they're enabled for interactions.
         await this.loginWithBapsSSOUserName.isEnabled();
         await this.loginWithBapsSSOPassword.isEnabled();
