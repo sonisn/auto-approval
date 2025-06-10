3a6598231fed86d07a9eeda996e7d5a89d294d9c
Snehal Soni <snehal.soni@farohealth.com>
2025-06-10 18:18:11 -0500

    Capture the screenshot when API fails.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
 import "tsconfig-paths";
-import {LoginPage} from "@pom/login.page";
-import {DashboardPage} from "@pom/dashboard.page";
-import {SearchPersonPage} from "@pom/search-person.page";
-import {DuplicatePersonModal} from "@pom/duplicate-person.modal";
-import {EditPersonModal} from "@pom/edit-person.modal";
-import { chromium} from "@playwright/test"
+import { LoginPage } from "@pom/login.page";
+import { DashboardPage } from "@pom/dashboard.page";
+import { SearchPersonPage } from "@pom/search-person.page";
+import { DuplicatePersonModal } from "@pom/duplicate-person.modal";
+import { EditPersonModal } from "@pom/edit-person.modal";
+import { chromium } from "@playwright/test";
 import logger from "./misc";
 
 // Load .env file for Local Run
-require('dotenv').config();
-
+require("dotenv").config();
 
 /*
     Starting point for the Script, as we use playwright API not as a test, launch browser using API.
@@ -19,9 +18,10 @@ require('dotenv').config();
  */
 
 (async () => {
-    const browser = await chromium.launch( { headless: true });
-    const page = await browser.newPage();
-    await page.goto('https://mis.na.baps.org/');
+  const browser = await chromium.launch({ headless: true });
+  const page = await browser.newPage();
+  try {
+    await page.goto("https://mis.na.baps.org/");
     const logInPage = new LoginPage(page);
     await logInPage.NavigateToLoginPage();
     await logInPage.LoginWithBapsSSO();
@@ -36,36 +36,67 @@ require('dotenv').config();
     logger.info(`Total Records to be processed: ${totalRecords}`);
 
     for (let rowNumber = 1; rowNumber <= totalRecords; rowNumber++) {
-        const misID = await searchPersonPage.resultTable.locator(searchPersonPage.resultRowLocator.replace('{rowNumber}', rowNumber.toString()))
-            .locator(searchPersonPage.misIdColumnLocator)
-            .innerText();
-        const firstName = await searchPersonPage.resultTable.locator(searchPersonPage.resultRowLocator.replace('{rowNumber}', rowNumber.toString()))
+      const misID = await searchPersonPage.resultTable
+        .locator(
+          searchPersonPage.resultRowLocator.replace(
+            "{rowNumber}",
+            rowNumber.toString()
+          )
+        )
+        .locator(searchPersonPage.misIdColumnLocator)
+        .innerText();
+      const firstName = await searchPersonPage.resultTable
+        .locator(
+          searchPersonPage.resultRowLocator.replace(
+            "{rowNumber}",
+            rowNumber.toString()
+          )
+        )
         .locator(searchPersonPage.firstNameColumnLocator)
         .innerText();
-        const lastName = await searchPersonPage.resultTable.locator(searchPersonPage.resultRowLocator.replace('{rowNumber}', rowNumber.toString()))
+      const lastName = await searchPersonPage.resultTable
+        .locator(
+          searchPersonPage.resultRowLocator.replace(
+            "{rowNumber}",
+            rowNumber.toString()
+          )
+        )
         .locator(searchPersonPage.lastNameColumnLocator)
         .innerText();
-        logger.info(`Working on MIS ID: ${misID}, First Name: ${firstName}, Last Name: ${lastName}`);
-        await searchPersonPage.clickDuplicatePerson(rowNumber);
-        const duplicatePersonModal = new DuplicatePersonModal(page);
-        if (await duplicatePersonModal.hasNoDataDisplayed()) {
-            logger.info(`No duplicate found hence approving record with MIS ID: ${misID}`);
-            await duplicatePersonModal.closeDuplicatePersonModal();
-            await searchPersonPage.clickEditPerson(rowNumber);
-            const editPersonModal = new EditPersonModal(page);
-            if (await editPersonModal.noEditPermissionDisplayed()) {
-                await editPersonModal.closeEditPersonModal();
-                logger.info(`Person with MIS ID: ${misID} can't approve as it has no edit permission.`);
-            } else {
-                await editPersonModal.clickApprove();
-                await editPersonModal.savePerson();
-                await editPersonModal.closeEditPersonModal();
-            }
+      logger.info(
+        `Working on MIS ID: ${misID}, First Name: ${firstName}, Last Name: ${lastName}`
+      );
+      await searchPersonPage.clickDuplicatePerson(rowNumber);
+      const duplicatePersonModal = new DuplicatePersonModal(page);
+      if (await duplicatePersonModal.hasNoDataDisplayed()) {
+        logger.info(
+          `No duplicate found hence approving record with MIS ID: ${misID}`
+        );
+        await duplicatePersonModal.closeDuplicatePersonModal();
+        await searchPersonPage.clickEditPerson(rowNumber);
+        const editPersonModal = new EditPersonModal(page);
+        if (await editPersonModal.noEditPermissionDisplayed()) {
+          await editPersonModal.closeEditPersonModal();
+          logger.info(
+            `Person with MIS ID: ${misID} can't approve as it has no edit permission.`
+          );
         } else {
-            await duplicatePersonModal.closeDuplicatePersonModal();
-            logger.info(`Duplicate found hence skipping approving record with MIS ID: ${misID}`);
+          await editPersonModal.clickApprove();
+          await editPersonModal.savePerson();
+          await editPersonModal.closeEditPersonModal();
         }
+      } else {
+        await duplicatePersonModal.closeDuplicatePersonModal();
+        logger.info(
+          `Duplicate found hence skipping approving record with MIS ID: ${misID}`
+        );
+      }
     }
     await dashboardPage.logout();
+  } catch (error) {
+    logger.error(`Error: ${error}`);
+    await page.screenshot({ path: "screenshots/error.png" });
+  } finally {
     await browser.close();
+  }
 })();
