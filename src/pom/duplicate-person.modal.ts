c53ddb8e711cf4d66b806ecb096cc91a70f38e10
Snehal Soni <snehal.soni@farohealth.com>
2025-06-24 20:22:35 -0500

    Refactor DuplicatePersonModal class for improved readability and error handling, including enhanced row count checks in hasNoDataDisplayed method.

--- src/pom/duplicate-person.modal.ts
+++ src/pom/duplicate-person.modal.ts
@@ -1,32 +1,40 @@
-import {Locator, Page} from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
 
 /*
     Represents Duplicate Person Modal Window when navigated from Search Result.
  */
 export class DuplicatePersonModal {
-    private readonly page: Page;
-    private readonly tableFirstRowContent: Locator;
-    private readonly closeBtn: Locator;
+  private readonly page: Page;
+  private readonly tableFirstRowContent: Locator;
+  private readonly closeBtn: Locator;
 
-    constructor(page: Page) {
-        this.page = page;
-        this.tableFirstRowContent = this.page.locator('baps-ui-duplicate-person-list table tbody tr:first-child td')
-        this.closeBtn = this.page.locator('baps-ui-duplicate-person-list button.close')
-    }
+  constructor(page: Page) {
+    this.page = page;
+    this.tableFirstRowContent = this.page.locator(
+      "baps-ui-duplicate-person-list table tbody tr:first-child td"
+    );
+    this.closeBtn = this.page.locator(
+      "baps-ui-duplicate-person-list button.close"
+    );
+  }
 
-    hasNoDataDisplayed = async () => {
-        await this.page.waitForLoadState("load");
-        await this.page.waitForLoadState("networkidle");
-        await this.page.waitForLoadState("domcontentloaded");
-        await this.page.waitForTimeout(5000);
-        if (await this.tableFirstRowContent.count() === 1) {
-            return await this.tableFirstRowContent.textContent() === 'No data to display';
-        } else if (await this.tableFirstRowContent.count() === 5) {
-            return false;
-        }
+  hasNoDataDisplayed = async () => {
+    await this.page.waitForLoadState("load");
+    await this.page.waitForLoadState("networkidle");
+    await this.page.waitForLoadState("domcontentloaded");
+    await this.page.waitForTimeout(5000);
+    if ((await this.tableFirstRowContent.count()) === 1) {
+      return (
+        (await this.tableFirstRowContent.textContent()) === "No data to display"
+      );
+    } else if ((await this.tableFirstRowContent.count()) === 5) {
+      return false;
+    } else {
+      throw new Error("Unexpected number of rows in duplicate person modal");
     }
+  };
 
-    closeDuplicatePersonModal = async () => {
-        await this.closeBtn.click();
-    }
+  closeDuplicatePersonModal = async () => {
+    await this.closeBtn.click();
+  };
 }
